4ee323775e473eb403cf88330d6d4f1ec1dd3483
Gandalfel <41485166+Gandalfel@users.noreply.github.com>
2018-08-28 17:38:18 +0200

    Update Window.java

--- Calculator/ViewAndDisplay/Window.java
+++ Calculator/ViewAndDisplay/Window.java
@@ -1,21 +1,26 @@
 package Calculator.ViewAndDisplay;
 
+import Calculator.Logic.Calculator;
+import Calculator.Logic.Converter;
 import Calculator.Logic.Filter;
 
-import static Calculator.Logic.Filter.deleteStatementSbContent;
+import static Calculator.Logic.Filter.*;
 import static Calculator.ViewAndDisplay.Display.deleteScreenContent;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import static Calculator.Logic.Filter.charsFromActionL;
-import static Calculator.Logic.Filter.eqalTo;
+import java.awt.datatransfer.*;
+import java.awt.Toolkit;
 
-public class Window extends JFrame {
+public class Window extends JFrame
+{
     protected static JTextArea screen;
-    private static Object source;
+    private static JMenuBar menuBar;
+    private static JMenu menuInfo, menuTools;
+    private static JMenuItem itemInfo, itemToolsCopy, itemToolsPaste, exit;
 
-    static String t0ext = "C", t6ext = "", t11ext = "", t17ext = "", t23ext = "",
+    private static String t0ext = "C", t6ext = "", t11ext = "", t17ext = "", t23ext = "",
             t1ext = "B", t7ext = "", t12ext = "", t18ext = "", t24ext = "",
             t2ext = "7", t8ext = "8", t13ext = "9", t19ext = "/", t25ext = "%",
             t3ext = "4", t9ext = "5", t14ext = "6", t20ext = "*", t26ext = "()",
@@ -35,18 +40,64 @@ public class Window extends JFrame {
 
     public Window()
     {
-        setSize(255, 320);
+        setSize(255, 307);
         setResizable(false);
         setTitle("Calculator");
         setLayout(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        //info
+        menuInfo = new JMenu("Info");
+
+        itemInfo = new JMenuItem("informations");
+        itemInfo.addActionListener(e ->
+        {
+            new InfoWindow();
+        });
+
+        menuBar.add(menuInfo);
+        menuInfo.add(itemInfo);
+
+        //tools
+        menuTools = new JMenu("Tools");
+
+        itemToolsCopy = new JMenuItem("copy (ctrl + c");
+        itemToolsCopy.addActionListener(e ->
+        {
+            String myString = screen.getText();
+            StringSelection stringSelection = new StringSelection(myString);
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clipboard.setContents(stringSelection, null);
+        });
+
+        itemToolsPaste = new JMenuItem("paste (ctrl + v)");
+        itemToolsPaste.addActionListener(e ->
+        {
+
+        });
+
+        //exit.setAccelerator(KeyStroke.getKeyStroke("ctrl x"));
+        exit = new JMenuItem("exit (ctrl + x)");
+        exit.addActionListener(e ->
+        {
+            dispose();
+        });
+
+        menuBar.add(menuTools);
+        menuTools.add(itemToolsCopy);
+        menuTools.add(itemToolsPaste);
+        menuTools.addSeparator();
+        menuTools.add(exit);
+
         screen = new JTextArea();
-        screen.setBounds(10, 40, 222, 50);
+        screen.setBounds(10, 10, 224, 50);
         screen.setEditable(false);
-        screen.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        screen.setFont(new Font("Arial", Font.ITALIC, 30));
+        screen.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        screen.setFont(new Font("Courier", Font.ITALIC, 28));
         screen.setLayout(null);
         screen.addKeyListener(new KeyListener()
         {
@@ -70,43 +121,45 @@ public class Window extends JFrame {
         });
         add(screen);
 
-        b0utton.setBounds(10, 95, 42, 25);
+        b0utton.setBounds(10, 65, 42, 25);
         b0utton.addActionListener(e ->
         {
             deleteScreenContent();
-            deleteStatementSbContent();
+            Filter.resetStatementSbAndResult();
+            Converter.resetResult();
+            Calculator.resetResult();
         });
         add(b0utton);
 
-        b1utton.setBounds(10, 125, 42, 25);
+        b1utton.setBounds(10, 95, 42, 25);
         b1utton.addActionListener(e ->
         {
             charsFromActionL(t1ext);
         });
         add(b1utton);
 
-        b2utton.setBounds(10, 155, 42, 25);
+        b2utton.setBounds(10, 125, 42, 25);
         b2utton.addActionListener(e ->
         {
             charsFromActionL(t2ext);
         });
         add(b2utton);
 
-        b3utton.setBounds(10, 185, 42, 25);
+        b3utton.setBounds(10, 155, 42, 25);
         b3utton.addActionListener(e ->
         {
             charsFromActionL(t3ext);
         });
         add(b3utton);
 
-        b4utton.setBounds(10, 215, 42, 25);
+        b4utton.setBounds(10, 185, 42, 25);
         b4utton.addActionListener(e ->
         {
             charsFromActionL(t4ext);
         });
         add(b4utton);
 
-        b5utton.setBounds(10, 245, 87, 25);
+        b5utton.setBounds(10, 215, 87, 25);
         b5utton.addActionListener(e ->
         {
             charsFromActionL(t5ext);
@@ -114,35 +167,35 @@ public class Window extends JFrame {
         add(b5utton);
 
         //druga kolumna
-        b6utton.setBounds(55, 95, 42, 25);
+        b6utton.setBounds(55, 65, 42, 25);
         b6utton.addActionListener(e ->
         {
             charsFromActionL(t6ext);
         });
         add(b6utton);
 
-        b7utton.setBounds(55, 125, 42, 25);
+        b7utton.setBounds(55, 95, 42, 25);
         b7utton.addActionListener(e ->
         {
             charsFromActionL(t7ext);
         });
         add(b7utton);
 
-        b8utton.setBounds(55, 155, 42, 25);
+        b8utton.setBounds(55, 125, 42, 25);
         b8utton.addActionListener(e ->
         {
             charsFromActionL(t8ext);
         });
         add(b8utton);
 
-        b9utton.setBounds(55, 185, 42, 25);
+        b9utton.setBounds(55, 155, 42, 25);
         b9utton.addActionListener(e ->
         {
             charsFromActionL(t9ext);
         });
         add(b9utton);
 
-        b10utton.setBounds(55, 215, 42, 25);
+        b10utton.setBounds(55, 185, 42, 25);
         b10utton.addActionListener(e ->
         {
             charsFromActionL(t10ext);
@@ -150,42 +203,42 @@ public class Window extends JFrame {
         add(b10utton);
 
         //trzecia kolumna
-        b11utton.setBounds(100, 95, 42, 25);
+        b11utton.setBounds(100, 65, 42, 25);
         b11utton.addActionListener(e ->
         {
             charsFromActionL(t11ext);
         });
         add(b11utton);
 
-        b12utton.setBounds(100, 125, 42, 25);
+        b12utton.setBounds(100, 95, 42, 25);
         b12utton.addActionListener(e ->
         {
             charsFromActionL(t12ext);
         });
         add(b12utton);
 
-        b13utton.setBounds(100, 155, 42, 25);
+        b13utton.setBounds(100, 125, 42, 25);
         b13utton.addActionListener(e ->
         {
             charsFromActionL(t13ext);
         });
         add(b13utton);
 
-        b14utton.setBounds(100, 185, 42, 25);
+        b14utton.setBounds(100, 155, 42, 25);
         b14utton.addActionListener(e ->
         {
             charsFromActionL(t14ext);
         });
         add(b14utton);
 
-        b15utton.setBounds(100, 215, 42, 25);
+        b15utton.setBounds(100, 185, 42, 25);
         b15utton.addActionListener(e ->
         {
             charsFromActionL(t15ext);
         });
         add(b15utton);
 
-        b16utton.setBounds(100, 245, 42, 25);
+        b16utton.setBounds(100, 215, 42, 25);
         b16utton.addActionListener(e ->
         {
             charsFromActionL(t16ext);
@@ -193,42 +246,42 @@ public class Window extends JFrame {
         add(b16utton);
 
         //czwarta kolumna
-        b17utton.setBounds(145, 95, 42, 25);
+        b17utton.setBounds(145, 65, 42, 25);
         b17utton.addActionListener(e ->
         {
             charsFromActionL(t17ext);
         });
         add(b17utton);
 
-        b18utton.setBounds(145, 125, 42, 25);
+        b18utton.setBounds(145, 95, 42, 25);
         b18utton.addActionListener(e ->
         {
             charsFromActionL(t18ext);
         });
         add(b18utton);
 
-        b19utton.setBounds(145, 155, 42, 25);
+        b19utton.setBounds(145, 125, 42, 25);
         b19utton.addActionListener(e ->
         {
             charsFromActionL(t19ext);
         });
         add(b19utton);
 
-        b20utton.setBounds(145, 185, 42, 25);
+        b20utton.setBounds(145, 155, 42, 25);
         b20utton.addActionListener(e ->
         {
             charsFromActionL(t20ext);
         });
         add(b20utton);
 
-        b21utton.setBounds(145, 215, 42, 25);
+        b21utton.setBounds(145, 185, 42, 25);
         b21utton.addActionListener(e ->
         {
             charsFromActionL(t21ext);
         });
         add(b21utton);
 
-        b22utton.setBounds(145, 245, 42, 25);
+        b22utton.setBounds(145, 215, 42, 25);
         b22utton.addActionListener(e ->
         {
             charsFromActionL(t22ext);
@@ -236,35 +289,35 @@ public class Window extends JFrame {
         add(b22utton);
 
         //piąta kolumna
-        b23utton.setBounds(190, 95, 42, 25);
+        b23utton.setBounds(190, 65, 42, 25);
         b23utton.addActionListener(e ->
         {
             charsFromActionL(t23ext);
         });
         add(b23utton);
 
-        b24utton.setBounds(190, 125, 42, 25);
+        b24utton.setBounds(190, 95, 42, 25);
         b24utton.addActionListener(e ->
         {
             charsFromActionL(t24ext);
         });
         add(b24utton);
 
-        b25utton.setBounds(190, 155, 42, 25);
+        b25utton.setBounds(190, 125, 42, 25);
         b25utton.addActionListener(e ->
         {
             charsFromActionL(t25ext);
         });
         add(b25utton);
 
-        b26utton.setBounds(190, 185, 42, 25);
+        b26utton.setBounds(190, 155, 42, 25);
         b26utton.addActionListener(e ->
         {
             charsFromActionL(t26ext);
         });
         add(b26utton);
 
-        b27utton.setBounds(190, 215, 42, 55);
+        b27utton.setBounds(190, 185, 42, 55);
         b27utton.addActionListener(e ->
         {
             eqalTo();
