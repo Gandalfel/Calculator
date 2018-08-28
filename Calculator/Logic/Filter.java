c031acf6e794c9d4c4c0b09ff1eacf8e6567c438
Gandalfel <41485166+Gandalfel@users.noreply.github.com>
2018-08-28 17:37:31 +0200

    Update Filter.java

--- Calculator/Logic/Filter.java
+++ Calculator/Logic/Filter.java
@@ -2,88 +2,122 @@ package Calculator.Logic;
 
 import java.awt.event.KeyEvent;
 import static Calculator.Logic.Calculator.isNumber;
+import static Calculator.Logic.Calculator.isOperator;
 import static Calculator.ViewAndDisplay.Display.displayingStrings;
 import static Calculator.ViewAndDisplay.Display.deleteScreenContent;
 
 public class Filter
 {
     private static String statement = "";
-    private static StringBuilder statementSb = new StringBuilder(statement);
     private static char keyChar;
     private static String keyString = "";
     private static String result = "";
-    private static boolean enter = false;
+    private static boolean isNumberDeleteScreenContent = false;
 
-    private static boolean isNumber = false;
-    private static boolean isOpeningBracket = false;
-    private static boolean isClosingBracket = false;
-
+    private static String buffer = "";
     public static void filtrating()
     {
-        if(keyString.equals("0") || keyString.equals("1") || keyString.equals("2") || keyString.equals("3") ||
-                keyString.equals("4") || keyString.equals("5") || keyString.equals("6") || keyString.equals("7") ||
-                keyString.equals("8") || keyString.equals("9") || keyString.equals("+") || keyString.equals("-") ||
-                keyString.equals("+") || keyString.equals("-"))
+        //System.out.println("statement.length(): " + statement.length());
+        System.out.println("buffer: " + buffer);
+        System.out.println("keyString: " + keyString);
+        if(isNumber(keyString) || isOperator(keyString)|| keyString.equals("*") ||
+                keyString.equals("/") || keyString.equals("(") || keyString.equals(")"))
         {
-            synchronized (statementSb)
+            if ((isNumber(keyString) || keyString.equals("(")) && statement.length() < 1)
             {
-                statementSb.append(keyString);
+                statement += keyString;
+                buffer = keyString;
                 displayingStrings(keyString);
-                System.out.print(keyChar);
             }
-        }
-        else if (keyString.equals("/"))
-        {
-            synchronized (statementSb)
+            else if (statement.length() >= 1)
             {
-                statementSb.append(keyString);
-                displayingStrings("÷");
-                System.out.print("÷");
-            }
-        }
-        else if (keyString.equals("*"))
-        {
-            synchronized (statementSb)
-            {
-                statementSb.append(keyString);
-                displayingStrings("×");
-                System.out.print("×");
-            }
-        }
-        else if (keyString.equals(",") || keyString.equals("."))
-        {
-            synchronized (statementSb)
-            {
-                statementSb.append(".");
-                displayingStrings(".");
-                System.out.print(".");
-            }
-        }
-        else if (keyString.equals("(") || keyString.equals(")"))
-        {
-            synchronized (statementSb)
-            {
-                statementSb.append(keyString);
-                displayingStrings(keyString);
-                System.out.print(keyString);
-            }
-        }
-        else if(keyChar == KeyEvent.VK_ENTER)
-        {
-            deleteScreenContent();
-            result += statementSb;
-            Converter.converting(Filter.getResult());
-            Calculator.calculating(Converter.getResult());
-            displayingStrings(Calculator.getResult());
+                if (isNumber(buffer) && isNumber(keyString))
+                {
+                    statement += keyString;
+                    buffer = keyString;
+                    displayingStrings(keyString);
+                }
+                else if (isNumber(buffer) && keyString.equals("("))
+                {
+                    statement += "*";
+                    statement += keyString;
+                    buffer = keyString;
+                    displayingStrings("×");
+                    displayingStrings(keyString);
+                }
+                else if (buffer.equals("(") && isOperator(keyString))
+                {
+                    //nothing
+                }
+                else if (buffer.equals("(") && isNumber(keyString))
+                {
+                    statement += keyString;
+                    buffer = keyString;
+                    displayingStrings(keyString);
+                }
+                else if (isNumber(buffer) && keyString.equals(")"))
+                {
+                    statement += keyString;
+                    buffer = keyString;
+                    displayingStrings(keyString);
+                }
+                else if (buffer.equals(")") && isNumber(keyString))
+                {
+                    statement += "*";
+                    statement += keyString;
+                    buffer = keyString;
+                    displayingStrings("×");
+                    displayingStrings(keyString);
+                }
+                else if (isNumber(buffer) && isOperator(keyString))
+                {
+                    statement += keyString;
+                    buffer = keyString;
+                    displayingStrings(keyString);
+                }
+                else if (isOperator(buffer) && isNumber(keyString))
+                {
+                    statement += keyString;
+                    buffer = keyString;
+                    displayingStrings(keyString);
+                }
+                else if (isOperator(buffer) && keyString.equals("("))
+                {
+                    statement += keyString;
+                    buffer = keyString;
+                    displayingStrings(keyString);
+                }
+                else if (buffer.equals(")") && isOperator(keyString))
+                {
+                    statement += keyString;
+                    buffer = keyString;
+                    displayingStrings(keyString);
+                }
+                else if(keyChar == KeyEvent.VK_ENTER)
+                {
+                    if (!isOperator(buffer) && (!buffer.equals("(") || !buffer.equals(")")))
+                    deleteScreenContent();
+                    result += statement;
+                    Converter.converting(Filter.getResult());
+                    Calculator.calculating(Converter.getResult());
+                    displayingStrings(Calculator.getResult());
 
-            System.out.println("\r");
-            System.out.println("FilterResult(): " + Filter.getResult());
-            System.out.println("ConverterResult(): " + Converter.getResult());
-            System.out.println("CalculatorResult(): " + Calculator.getResult());
-            statementSb.delete(0, statementSb.length());
-
-            result = "";
+                    System.out.println("\r");
+                    System.out.println("FilterResult(): " + Filter.getResult());
+                    System.out.println("ConverterResult(): " + Converter.getResult());
+                    System.out.println("CalculatorResult(): " + Calculator.getResult());
+                    statement = "";
+                    result = "";
+                }
+            }
         }
+        System.out.println("statement.length(): " + statement.length());
+    }
+
+    public static void resetStatementSbAndResult()
+    {
+        statement = "";
+        result = "";
     }
 
     public static void charsFromKeyL(KeyEvent e)
@@ -91,26 +125,21 @@ public class Filter
         keyString = "";
         keyChar = e.getKeyChar();
         keyString += keyChar;
-        System.out.print(keyString);
+        //System.out.print(keyString);
         filtrating();
     }
 
     public static void charsFromActionL(String a)
     {
         keyString = a;
-        System.out.print(keyString);
+        //System.out.print(keyString);
         filtrating();
-    }
-
-    public static void deleteStatementSbContent()
-    {
-        statementSb.delete(0, statementSb.length());
     }
 
     public static void eqalTo()
     {
         deleteScreenContent();
-        result += statementSb;
+        result += statement;
         Converter.converting(Filter.getResult());
         Calculator.calculating(Converter.getResult());
         displayingStrings(Calculator.getResult());
@@ -119,43 +148,5 @@ public class Filter
     public static String getResult()
     {
         return result;
-    }
-
-    public static class AdditionMultiplicationSign  implements Runnable
-    {
-        public AdditionMultiplicationSign()
-        {
-            run();
-        }
-
-        public void run()
-        {
-            for(int a = 0;a == statementSb.length();a++)
-            {
-                if (isNumber(statementSb.charAt(a)) == true ||
-                        statementSb.charAt(a - 1) == ')') {
-                    String numberChar = String.valueOf(statementSb.charAt(a));
-                    synchronized (statementSb)
-                    {
-                        statementSb.deleteCharAt(a);
-                        statementSb.append('*');
-                        statementSb.append(numberChar);
-                        System.out.println("ten wątek wstawiający dodatkowy znak dzielenia");
-                    }
-                }
-                else if (statementSb.charAt(a) == '(' ||
-                        isNumber(statementSb.charAt(a - 1)) == true)
-                {
-                    String numberChar = String.valueOf(statementSb.charAt(a));
-                    synchronized (statementSb)
-                    {
-                        statementSb.deleteCharAt(a);
-                        statementSb.append('*');
-                        statementSb.append(numberChar);
-                    }
-                    result += statementSb;
-                }
-            }
-        }
     }
 }
