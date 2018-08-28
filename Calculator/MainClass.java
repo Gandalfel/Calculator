9a1383debbf8e2eb7b83f241abbb7f10299b75b7
Gandalfel <41485166+Gandalfel@users.noreply.github.com>
2018-08-28 20:32:22 +0200

    Update MainClass.java

--- Calculator/MainClass.java
+++ Calculator/MainClass.java
@@ -1,8 +1,6 @@
-package Calculator;
+package Calculator.Logic;
 
-import Calculator.Logic.Filter;
 import Calculator.ViewAndDisplay.Window;
-import javax.swing.*;
 
 public class MainClass
 {
@@ -10,20 +8,5 @@ public class MainClass
     {
         Window window = new Window();
         Filter filter = new Filter();
-        try
-        {
-            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
-            {
-                if ("Nimbus".equals(info.getName()))
-                {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println("LookAmdFeel Error");
-        }
     }
 }
